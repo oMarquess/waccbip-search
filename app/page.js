229900331e7c517167a4f3dd02c67175661b9763
oMarquess <olufemimarquess@gmail.com>
2026-02-23 12:27:57 +0000

    style: constrain textLength on circular banner to fix alignment

--- app/page.js
+++ app/page.js
@@ -6,23 +6,9 @@ export default function Home() {
     <main className={styles.main}>
       <div>
         <h1>Secure sensitive customer data</h1>
-        <p>
-          Evervault is an encryption and data security platform for developers.
-          Using Evervault allows you to keep sensitive data encrypted at all
-          times, without compromising your ability to use that data.
-        </p>
-        <p>
-          WACCBIP Search is a Next.js app with Evervault installed. Each page
-          includes a sandbox of each Evervault product to help you get up to
-          speed with the platform. During the integration, we created some
-          Evervault products for you to use, and injected them into this project
-          as environment variables.
-        </p>
-        <p>
-          Get started by selecting which product you’d like to explore first.
-        </p>
+        
       </div>
-      <QuickLinks />
+      {/* <QuickLinks /> */}
     </main>
   );
 }
